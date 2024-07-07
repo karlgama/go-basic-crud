3743ad113da85cc84b4479903200367d63d5be1b
kaique <kaiquesilva49@hotmail.com>
2024-07-07 20:07:16 -0300

    setup the service

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/karlgama/go-microservices/internal/database"
+	"github.com/karlgama/go-microservices/internal/server"
+)
+
+func main() {
+	db, err := database.NewDatabaseClient()
+	if err != nil {
+		log.Fatalf("failed to initialize Database Client: %s", err)
+	}
+	serv := server.NewEchoServer(db)
+	if err := serv.Start(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
